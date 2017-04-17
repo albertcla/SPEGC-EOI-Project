7bb9ea56db90e34f042d4b6ba5f932075dd656fb
tucho-lenovo <albert.cla@gmail.com>
2017-04-17 23:46:09 +0100

    Creación de filtros tipo range
    Se ha sustituido la imagen inicial de los filtros por cuatro inputs tipo
    rango. En un futuro estos inputs deberán tener dos selectores, en
    principio se ha creado solamente uno.

---
+++ js/range-value.js
@@ -0,0 +1,11 @@
+var w = document.getElementById('year').value;
+document.getElementById('range-year').innerHTML = w;
+
+var x = document.getElementById('imdb').value;
+document.getElementById('range-imdb').innerHTML = x;
+
+var y = document.getElementById('cs').value;
+document.getElementById('range-cs').innerHTML = y;
+
+var z = document.getElementById('as').value;
+document.getElementById('range-as').innerHTML = z;
